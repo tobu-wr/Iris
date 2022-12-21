278490bcbbee71a35932c7de0f047c9f841d7b83
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2022-12-21 22:23:41 +0000

    PPU: add dispstat register and RAM

--- Iris/PPU.cs
+++ Iris/PPU.cs
@@ -8,6 +8,11 @@ namespace Iris
 {
     public class PPU
     {
+        public Byte[] paletteRAM = new Byte[1 * 1024]; // 1 KB
+        //public Byte[] VRAM = new Byte[96 * 1024]; // 96 KB
+
+        public UInt16 dispstat = 0;
+
         // private const UInt32 SCREEN_WIDTH = 240;
         private const UInt32 SCREEN_HEIGHT = 160;
         private const UInt32 HORIZONTAL_LINE_WIDTH = 308;
@@ -22,11 +27,16 @@ namespace Iris
                 // start of vertical blank
                 // TODO
                 ++cycleCounter;
+                dispstat |= 1;
             }
             else if (cycleCounter == HORIZONTAL_LINE_WIDTH * HORIZONTAL_LINE_COUNT)
             {
                 // end of vertical blank
                 // TODO
+                unchecked
+                {
+                    dispstat &= (UInt16)~1; 
+                }
                 cycleCounter = 0;
             }
             else
