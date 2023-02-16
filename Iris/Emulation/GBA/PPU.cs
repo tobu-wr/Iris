117b7e8449d2bcd197dde61c7917e00efee218a5
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2023-02-16 19:12:49 +0000

    PPU update

--- Iris/Emulation/GBA/PPU.cs
+++ Iris/Emulation/GBA/PPU.cs
@@ -1,52 +1,52 @@
-﻿namespace Iris.Core
+﻿namespace Iris.Emulation.GBA
 {
-    public class PPU
+    internal sealed class PPU
     {
         private const int KB = 1024;
 
-        public byte[] PaletteRAM = new byte[1 * KB];
-        public byte[] VRAM = new byte[96 * KB];
+        internal Byte[] PaletteRAM = new Byte[1 * KB];
+        internal Byte[] VRAM = new Byte[96 * KB];
 
-        public ushort DISPSTAT = 0;
-        public ushort DISPCNT = 0;
-        public ushort VCOUNT = 0;
+        internal UInt16 DISPSTAT = 0;
+        internal UInt16 DISPCNT = 0;
+        internal UInt16 VCOUNT = 0;
 
-        private const uint ScreenWidth = 240;
-        private const uint ScreenHeight = 160;
-        private const uint HorizontalLineWidth = 308;
-        private const uint HorizontalLineCount = 228;
+        private const UInt32 ScreenWidth = 240;
+        private const UInt32 ScreenHeight = 160;
+        private const UInt32 HorizontalLineWidth = 308;
+        private const UInt32 HorizontalLineCount = 228;
 
         private readonly IRenderer _renderer;
-        private uint _cycleCounter = 0;
+        private UInt32 _cycleCounter = 0;
 
-        public PPU(IRenderer renderer)
+        internal PPU(IRenderer renderer)
         {
             _renderer = renderer;
         }
 
-        public void Step()
+        internal void Step()
         {
-            VCOUNT = (ushort)(_cycleCounter / HorizontalLineWidth);
+            VCOUNT = (UInt16)(_cycleCounter / HorizontalLineWidth);
 
             if (_cycleCounter == HorizontalLineWidth * ScreenHeight)
             {
                 // start of vertical blank
-                ushort bgMode = (ushort)(DISPCNT & 0b111);
+                UInt16 bgMode = (UInt16)(DISPCNT & 0b111);
                 switch (bgMode)
                 {
                     case 0b100:
                         {
-                            ushort bg2 = (ushort)(DISPCNT >> 10 & 1);
+                            UInt16 bg2 = (UInt16)(DISPCNT >> 10 & 1);
                             if (bg2 == 1)
                             {
-                                ushort frameBuffer = (ushort)(DISPCNT >> 4 & 1);
-                                uint frameBufferAddress = frameBuffer == 0 ? 0x0_0000u : 0x0_a000u;
+                                UInt16 frameBuffer = (UInt16)(DISPCNT >> 4 & 1);
+                                UInt32 frameBufferAddress = frameBuffer == 0 ? 0x0_0000u : 0x0_a000u;
 
-                                ushort[] rendererFrameBuffer = new ushort[ScreenWidth * ScreenHeight];
-                                for (uint i = 0; i < ScreenWidth * ScreenHeight; ++i)
+                                UInt16[] rendererFrameBuffer = new UInt16[ScreenWidth * ScreenHeight];
+                                for (UInt32 i = 0; i < ScreenWidth * ScreenHeight; ++i)
                                 {
-                                    byte colorNo = VRAM[frameBufferAddress + i];
-                                    ushort color = (ushort)(PaletteRAM[colorNo * 2 + 1] << 8
+                                    Byte colorNo = VRAM[frameBufferAddress + i];
+                                    UInt16 color = (UInt16)(PaletteRAM[colorNo * 2 + 1] << 8
                                                           | PaletteRAM[colorNo * 2 + 0] << 0);
                                     rendererFrameBuffer[i] = color;
                                 }
