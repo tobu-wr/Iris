5c1febb9e56074d7cf8cde9035768f8a6ee27534
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2023-02-19 15:05:47 +0000

    fix bios irq handler

--- Iris/Emulation/GBA/BIOS.cs
+++ Iris/Emulation/GBA/BIOS.cs
@@ -13,7 +13,6 @@
 
             _cpu.Reg[CPU.Core.SP] = 0x0300_7f00;
             _cpu.Reg[CPU.Core.LR] = ROMAddress;
-            _cpu.Reg[CPU.Core.PC] = ROMAddress;
 
             _cpu.Reg13_svc = 0x0300_7fe0;
             _cpu.Reg13_irq = 0x0300_7fa0;
@@ -50,7 +49,6 @@
         {
             _cpu.Reg14_irq = _cpu.NextInstructionAddress + 4;
             _cpu.SPSR_irq = _cpu.CPSR;
-
             _cpu.SetCPSR((_cpu.CPSR & 0xffff_ff40) | 0x92);
 
             _cpu.Reg[CPU.Core.SP] -= 4;
@@ -66,14 +64,9 @@
             _cpu.Reg[CPU.Core.SP] -= 4;
             WriteMemory32(_cpu.Reg[CPU.Core.SP], _cpu.Reg[CPU.Core.LR]);
 
-            const UInt32 UserHandlerAddress = 0x0300_7ffc;
-
             _cpu.Reg[0] = 0x0400_0000;
-
             _cpu.Reg[CPU.Core.LR] = 0x0000_0138;
-            _cpu.Reg[CPU.Core.PC] = UserHandlerAddress;
-
-            _cpu.NextInstructionAddress = UserHandlerAddress;
+            _cpu.NextInstructionAddress = ReadMemory32(0x0300_7ffc);
         }
 
         private void Div()
