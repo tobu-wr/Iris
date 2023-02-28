3928e94adcf24d920a7b94dd97bc57a485800f20
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2023-02-28 19:35:55 +0000

    Add InterruptControl.cs

---
+++ Iris/Emulation/GBA/InterruptControl.cs
@@ -0,0 +1,30 @@
+﻿namespace Iris.Emulation.GBA
+{
+    internal sealed partial class Core
+    {
+        private UInt16 _IE;
+        private UInt16 _IF;
+        private UInt16 _IME;
+
+        private void RequestVBlankInterrupt()
+        {
+            _IF |= 1;
+            UpdateInterrupts();
+        }
+
+        private void UpdateInterrupts()
+        {
+            if (_IME == 1)
+            {
+                if ((_IE & _IF & 1) != 0) // VBlank
+                    _CPU.NIRQ = CPU.Core.Signal.Low;
+                else
+                    _CPU.NIRQ = CPU.Core.Signal.High;
+            }
+            else
+            {
+                _CPU.NIRQ = CPU.Core.Signal.High;
+            }
+        }
+    }
+}
