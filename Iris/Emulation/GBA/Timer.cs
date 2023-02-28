a5ad8af3381c3279d6d269b7df6789db93ebd46d
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2023-02-28 21:50:47 +0000

    add Timer.cs

---
+++ Iris/Emulation/GBA/Timer.cs
@@ -0,0 +1,17 @@
+﻿namespace Iris.Emulation.GBA
+{
+    internal sealed partial class Core
+    {
+        private UInt16 _TM0CNT_L;
+        private UInt16 _TM0CNT_H;
+
+        private UInt16 _TM1CNT_L;
+        private UInt16 _TM1CNT_H;
+
+        private UInt16 _TM2CNT_L;
+        private UInt16 _TM2CNT_H;
+
+        private UInt16 _TM3CNT_L;
+        private UInt16 _TM3CNT_H;
+    }
+}
