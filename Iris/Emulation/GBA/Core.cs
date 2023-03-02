eca0fe62f175883ac1a059fb68de07fcc11acc54
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2023-03-02 12:37:26 +0000

    update gba core

--- Iris/Emulation/GBA/Core.cs
+++ Iris/Emulation/GBA/Core.cs
@@ -2,95 +2,10 @@
 {
     internal sealed partial class Core
     {
-        internal enum Keys
-        {
-            A = 0,
-            B = 1,
-            Select = 2,
-            Start = 3,
-            Right = 4,
-            Left = 5,
-            Up = 6,
-            Down = 7,
-            R = 8,
-            L = 9,
-        };
-
         private readonly CPU.Core _CPU;
         private readonly PPU _PPU;
 
-        private UInt16 _SOUND1CNT_H;
-        private UInt16 _SOUND1CNT_X;
-
-        private UInt16 _SOUND2CNT_L;
-        private UInt16 _SOUND2CNT_H;
-
-        private UInt16 _SOUND3CNT_L;
-
-        private UInt16 _SOUND4CNT_L;
-        private UInt16 _SOUND4CNT_H;
-
-        private UInt16 _SOUNDCNT_L;
-        private UInt16 _SOUNDCNT_H;
-        private UInt16 _SOUNDCNT_X;
-
-        private UInt16 _SOUNDBIAS;
-
-        private UInt16 _DMA0CNT_H;
-
-        private UInt16 _DMA1SAD_L;
-        private UInt16 _DMA1SAD_H;
-
-        private UInt16 _DMA1DAD_L;
-        private UInt16 _DMA1DAD_H;
-
-        private UInt16 _DMA1CNT_L;
-        private UInt16 _DMA1CNT_H;
-
-        private UInt16 _DMA2SAD_L;
-        private UInt16 _DMA2SAD_H;
-
-        private UInt16 _DMA2DAD_L;
-        private UInt16 _DMA2DAD_H;
-
-        private UInt16 _DMA2CNT_L;
-        private UInt16 _DMA2CNT_H;
-
-        //private UInt16 _DMA3SAD_L;
-        //private UInt16 _DMA3SAD_H;
-        //private UInt16 _DMA3DAD_L;
-        //private UInt16 _DMA3DAD_H;
-        private UInt16 _DMA3CNT_H;
-
-        private UInt16 _TM0CNT_L;
-        private UInt16 _TM0CNT_H;
-
-        private UInt16 _TM1CNT_L;
-        private UInt16 _TM1CNT_H;
-
-        private UInt16 _TM2CNT_L;
-        private UInt16 _TM2CNT_H;
-
-        private UInt16 _TM3CNT_L;
-        private UInt16 _TM3CNT_H;
-
-        private UInt16 _SIOMULTI0;
-        private UInt16 _SIOMULTI1;
-        private UInt16 _SIOMULTI2;
-        private UInt16 _SIOMULTI3;
-
-        private UInt16 _SIOCNT;
-        private UInt16 _SIODATA8;
-
-        private UInt16 _KEYINPUT;
-        private UInt16 _KEYCNT;
-
-        private UInt16 _RCNT;
-
-        private UInt16 _IE;
-        private UInt16 _IF;
         private UInt16 _WAITCNT;
-        private UInt16 _IME;
 
         private bool _running;
 
@@ -111,7 +26,7 @@
             PPU.CallbackInterface ppuCallbackInterface = new()
             {
                 DrawFrame = drawFrame,
-                RequestVBlankInterrupt = RequestVBlankInterrupt
+                RequestInterrupt = RequestInterrupt
             };
 
             _CPU = new(CPU.Core.Architecture.ARMv4T, cpuCallbackInterface);
@@ -172,33 +87,6 @@
         internal void Pause()
         {
             _running = false;
-        }
-
-        internal void SetKeyStatus(Keys key, bool pressed)
-        {
-            int mask = 1 << (int)key;
-            _KEYINPUT = (UInt16)(pressed ? (_KEYINPUT & ~mask) : (_KEYINPUT | mask));
-        }
-
-        private void RequestVBlankInterrupt()
-        {
-            _IF |= 1;
-            UpdateInterrupts();
-        }
-
-        private void UpdateInterrupts()
-        {
-            if (_IME == 1)
-            {
-                if ((_IE & _IF & 1) != 0) // VBlank
-                    _CPU.NIRQ = CPU.Core.Signal.Low;
-                else
-                    _CPU.NIRQ = CPU.Core.Signal.High;
-            }
-            else
-            {
-                _CPU.NIRQ = CPU.Core.Signal.High;
-            }
         }
     }
 }
