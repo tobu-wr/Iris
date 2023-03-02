b81de11120a6dea38b5213510e8ba90515397738
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2023-03-02 12:03:34 +0000

    update DMA

---
+++ Iris/Emulation/GBA/Core.DMA.cs
@@ -0,0 +1,41 @@
+﻿namespace Iris.Emulation.GBA
+{
+    internal sealed partial class Core
+    {
+        //private UInt16 _DMA0SAD_L;
+        //private UInt16 _DMA0SAD_H;
+
+        //private UInt16 _DMA0DAD_L;
+        //private UInt16 _DMA0DAD_H;
+
+        //private UInt16 _DMA0CNT_L;
+        private UInt16 _DMA0CNT_H;
+
+        private UInt16 _DMA1SAD_L;
+        private UInt16 _DMA1SAD_H;
+
+        private UInt16 _DMA1DAD_L;
+        private UInt16 _DMA1DAD_H;
+
+        private UInt16 _DMA1CNT_L;
+        private UInt16 _DMA1CNT_H;
+
+        private UInt16 _DMA2SAD_L;
+        private UInt16 _DMA2SAD_H;
+
+        private UInt16 _DMA2DAD_L;
+        private UInt16 _DMA2DAD_H;
+
+        private UInt16 _DMA2CNT_L;
+        private UInt16 _DMA2CNT_H;
+
+        //private UInt16 _DMA3SAD_L;
+        //private UInt16 _DMA3SAD_H;
+
+        //private UInt16 _DMA3DAD_L;
+        //private UInt16 _DMA3DAD_H;
+
+        //private UInt16 _DMA3CNT_L;
+        private UInt16 _DMA3CNT_H;
+    }
+}
