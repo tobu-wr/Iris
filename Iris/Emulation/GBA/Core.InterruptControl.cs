e11ff8e0fe5cbdacd6b4b9eeead063c00634d75b
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2023-03-02 12:07:11 +0000

    update interrupts

--- Iris/Emulation/GBA/Core.InterruptControl.cs
+++ Iris/Emulation/GBA/Core.InterruptControl.cs
@@ -5,6 +5,19 @@
         internal enum Interrupt
         {
             VBlank = 1 << 0,
+            //HBlank = 1 << 1,
+            //VCounterMatch = 1 << 2,
+            //Timer0 = 1 << 3,
+            //Timer1 = 1 << 4,
+            //Timer2 = 1 << 5,
+            //Timer3 = 1 << 6,
+            //SIO = 1 << 7,
+            //DMA0 = 1 << 8,
+            //DMA1 = 1 << 9,
+            //DMA2 = 1 << 10,
+            //DMA3 = 1 << 11,
+            //Key = 1 << 12,
+            //GamePak = 1 << 13,
         }
 
         private UInt16 _IE;
