f1201f0cdd1b524303bdd77b6a93f13cfa063431
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2023-03-09 20:58:01 +0000

    emulation core: add ICore interface

---
+++ Iris.EmulationCore/Common/ICore.cs
@@ -0,0 +1,13 @@
+﻿namespace Iris.EmulationCore.Common
+{
+    public delegate void DrawFrame_Delegate(UInt16[] frameBuffer);
+
+    public interface ICore
+    {
+        public void Reset();
+        public void LoadROM(string filename);
+        public bool IsRunning();
+        public void Run();
+        public void Pause();
+    }
+}
