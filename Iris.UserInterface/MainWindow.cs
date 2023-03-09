3fedbb630448474c3d41cab67bac4593d4727e4c
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2023-03-09 17:10:56 +0000

    update userinterface

--- Iris.UserInterface/MainWindow.cs
+++ Iris.UserInterface/MainWindow.cs
@@ -1,3 +1,6 @@
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
 namespace Iris.UserInterface
 {
     public partial class MainWindow : Form
@@ -21,51 +24,192 @@ namespace Iris.UserInterface
         private int _frameCount = 0;
         private readonly System.Timers.Timer _performanceUpdateTimer = new(1000);
 
-        public MainWindow()
+        public MainWindow(string[] args)
         {
             InitializeComponent();
 
             _GBA = new(DrawFrame);
+
+            _performanceUpdateTimer.Elapsed += new System.Timers.ElapsedEventHandler(PerformanceUpdateTimer_Elapsed);
+
+            if (args.Length > 0 && LoadROM(args[0]))
+            {
+                loadStateToolStripMenuItem.Enabled = true;
+                saveStateToolStripMenuItem.Enabled = true;
+                runToolStripMenuItem.Enabled = true;
+                pauseToolStripMenuItem.Enabled = false;
+                restartToolStripMenuItem.Enabled = true;
+                toolStripStatusLabel1.Text = "Paused";
+            }
         }
 
         private void DrawFrame(UInt16[] frameBuffer)
         {
+            const int ScreenWidth = 240;
+            const int ScreenHeight = 160;
+            const PixelFormat PixelFormat = PixelFormat.Format16bppRgb555;
 
+            Bitmap bitmap = new(ScreenWidth, ScreenHeight, PixelFormat);
+            BitmapData data = bitmap.LockBits(new Rectangle(0, 0, ScreenWidth, ScreenHeight), ImageLockMode.WriteOnly, PixelFormat);
+
+            const int PixelCount = ScreenWidth * ScreenHeight;
+            Int16[] buffer = new Int16[PixelCount];
+            for (int i = 0; i < PixelCount; ++i)
+            {
+                UInt16 gbaColor = frameBuffer[i]; // BGR format
+                Byte red = (Byte)((gbaColor >> 0) & 0x1f);
+                Byte green = (Byte)((gbaColor >> 5) & 0x1f);
+                Byte blue = (Byte)((gbaColor >> 10) & 0x1f);
+                buffer[i] = (Int16)((red << 10) | (green << 5) | blue);
+            }
+
+            System.Runtime.InteropServices.Marshal.Copy(buffer, 0, data.Scan0, PixelCount);
+            bitmap.UnlockBits(data);
+            screenBox.Invoke(() => screenBox.Image = bitmap);
+            screenBox.Invalidate();
+
+            ++_frameCount;
+        }
+
+        private bool LoadROM(string fileName)
+        {
+            try
+            {
+                _GBA.LoadROM(fileName);
+                _GBA.Reset();
+                return true;
+            }
+            catch
+            {
+                MessageBox.Show("Could not load ROM", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
+        private void Run()
+        {
+            runToolStripMenuItem.Enabled = false;
+            pauseToolStripMenuItem.Enabled = true;
+            toolStripStatusLabel1.Text = "Running";
+
+            Task.Run(() =>
+            {
+                try
+                {
+                    _GBA.Run();
+                }
+                catch (Exception ex)
+                {
+                    Pause();
+                    _GBA.Reset();
+                    MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            });
+
+            _performanceUpdateTimer.Start();
+        }
+
+        private void Pause()
+        {
+            runToolStripMenuItem.Enabled = true;
+            pauseToolStripMenuItem.Enabled = false;
+            toolStripStatusLabel1.Text = "Paused";
+            _GBA.Pause();
+
+            _performanceUpdateTimer.Stop();
+            toolStripStatusLabel3.Text = "FPS: 0";
+            _frameCount = 0;
         }
 
         private void loadROMToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            bool running = _GBA.IsRunning();
+            if (running)
+                Pause();
 
+            OpenFileDialog dialog = new();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                if (LoadROM(dialog.FileName) && !running)
+                {
+                    loadStateToolStripMenuItem.Enabled = true;
+                    saveStateToolStripMenuItem.Enabled = true;
+                    runToolStripMenuItem.Enabled = true;
+                    pauseToolStripMenuItem.Enabled = false;
+                    restartToolStripMenuItem.Enabled = true;
+                    toolStripStatusLabel1.Text = "Paused";
+                }
+            }
+
+            if (running)
+                Run();
         }
 
         private void loadStateToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            bool running = _GBA.IsRunning();
+            if (running)
+                Pause();
 
+            OpenFileDialog dialog = new();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                // TODO
+            }
+
+            if (running)
+                Run();
         }
 
         private void saveStateToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            bool running = _GBA.IsRunning();
+            if (running)
+                Pause();
 
+            SaveFileDialog dialog = new();
+            if (dialog.ShowDialog(this) == DialogResult.OK)
+            {
+                // TODO
+            }
+
+            if (running)
+                Run();
         }
 
         private void quitToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            Application.Exit();
         }
 
         private void runToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            Run();
         }
 
         private void pauseToolStripMenuItem_Click(object sender, EventArgs e)
         {
-
+            Pause();
         }
 
         private void restartToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            bool running = _GBA.IsRunning();
+            if (running)
+                Pause();
 
+            _GBA.Reset();
+
+            if (running)
+                Run();
+        }
+
+        private void PerformanceUpdateTimer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
+        {
+            int fps = (int)(_frameCount * 1000 / _performanceUpdateTimer.Interval);
+            menuStrip1.Invoke(() => toolStripStatusLabel3.Text = "FPS: " + fps);
+            Console.WriteLine("UserInterface.MainWindow: FPS: {0}", fps);
+            _frameCount = 0;
         }
     }
 }
