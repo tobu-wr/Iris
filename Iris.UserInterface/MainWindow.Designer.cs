d1f0b0748ad67a6cfd4f9772d862e730b59594dc
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2023-03-09 12:44:39 +0000

    update user interface

--- Iris.UserInterface/MainWindow.Designer.cs
+++ Iris.UserInterface/MainWindow.Designer.cs
@@ -28,12 +28,172 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            statusStrip1 = new StatusStrip();
+            toolStripStatusLabel1 = new ToolStripStatusLabel();
+            toolStripStatusLabel2 = new ToolStripStatusLabel();
+            toolStripStatusLabel3 = new ToolStripStatusLabel();
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            loadROMToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator1 = new ToolStripSeparator();
+            loadStateToolStripMenuItem = new ToolStripMenuItem();
+            saveStateToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator2 = new ToolStripSeparator();
+            quitToolStripMenuItem = new ToolStripMenuItem();
+            emulationToolStripMenuItem = new ToolStripMenuItem();
+            runToolStripMenuItem = new ToolStripMenuItem();
+            pauseToolStripMenuItem = new ToolStripMenuItem();
+            restartToolStripMenuItem = new ToolStripMenuItem();
+            statusStrip1.SuspendLayout();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            statusStrip1.Items.AddRange(new ToolStripItem[] { toolStripStatusLabel1, toolStripStatusLabel2, toolStripStatusLabel3 });
+            statusStrip1.Location = new Point(0, 428);
+            statusStrip1.Name = "statusStrip1";
+            statusStrip1.Size = new Size(800, 22);
+            statusStrip1.TabIndex = 0;
+            statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            toolStripStatusLabel1.Size = new Size(92, 17);
+            toolStripStatusLabel1.Text = "No ROM loaded";
+            // 
+            // toolStripStatusLabel2
+            // 
+            toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            toolStripStatusLabel2.Size = new Size(12, 17);
+            toolStripStatusLabel2.Text = "-";
+            // 
+            // toolStripStatusLabel3
+            // 
+            toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            toolStripStatusLabel3.Size = new Size(38, 17);
+            toolStripStatusLabel3.Text = "FPS: 0";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, emulationToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { loadROMToolStripMenuItem, toolStripSeparator1, loadStateToolStripMenuItem, saveStateToolStripMenuItem, toolStripSeparator2, quitToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "&File";
+            // 
+            // loadROMToolStripMenuItem
+            // 
+            loadROMToolStripMenuItem.Name = "loadROMToolStripMenuItem";
+            loadROMToolStripMenuItem.Size = new Size(139, 22);
+            loadROMToolStripMenuItem.Text = "Load &ROM...";
+            loadROMToolStripMenuItem.Click += loadROMToolStripMenuItem_Click;
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(136, 6);
+            // 
+            // loadStateToolStripMenuItem
+            // 
+            loadStateToolStripMenuItem.Name = "loadStateToolStripMenuItem";
+            loadStateToolStripMenuItem.Size = new Size(139, 22);
+            loadStateToolStripMenuItem.Text = "&Load state...";
+            loadStateToolStripMenuItem.Click += loadStateToolStripMenuItem_Click;
+            // 
+            // saveStateToolStripMenuItem
+            // 
+            saveStateToolStripMenuItem.Name = "saveStateToolStripMenuItem";
+            saveStateToolStripMenuItem.Size = new Size(139, 22);
+            saveStateToolStripMenuItem.Text = "&Save state...";
+            saveStateToolStripMenuItem.Click += saveStateToolStripMenuItem_Click;
+            // 
+            // toolStripSeparator2
+            // 
+            toolStripSeparator2.Name = "toolStripSeparator2";
+            toolStripSeparator2.Size = new Size(136, 6);
+            // 
+            // quitToolStripMenuItem
+            // 
+            quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            quitToolStripMenuItem.Size = new Size(139, 22);
+            quitToolStripMenuItem.Text = "&Quit";
+            quitToolStripMenuItem.Click += quitToolStripMenuItem_Click;
+            // 
+            // emulationToolStripMenuItem
+            // 
+            emulationToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { runToolStripMenuItem, pauseToolStripMenuItem, restartToolStripMenuItem });
+            emulationToolStripMenuItem.Name = "emulationToolStripMenuItem";
+            emulationToolStripMenuItem.Size = new Size(73, 20);
+            emulationToolStripMenuItem.Text = "&Emulation";
+            // 
+            // runToolStripMenuItem
+            // 
+            runToolStripMenuItem.Name = "runToolStripMenuItem";
+            runToolStripMenuItem.Size = new Size(110, 22);
+            runToolStripMenuItem.Text = "R&un";
+            runToolStripMenuItem.Click += runToolStripMenuItem_Click;
+            // 
+            // pauseToolStripMenuItem
+            // 
+            pauseToolStripMenuItem.Name = "pauseToolStripMenuItem";
+            pauseToolStripMenuItem.Size = new Size(110, 22);
+            pauseToolStripMenuItem.Text = "&Pause";
+            pauseToolStripMenuItem.Click += pauseToolStripMenuItem_Click;
+            // 
+            // restartToolStripMenuItem
+            // 
+            restartToolStripMenuItem.Name = "restartToolStripMenuItem";
+            restartToolStripMenuItem.Size = new Size(110, 22);
+            restartToolStripMenuItem.Text = "R&estart";
+            restartToolStripMenuItem.Click += restartToolStripMenuItem_Click;
+            // 
+            // MainWindow
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(statusStrip1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "MainWindow";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Iris";
+            statusStrip1.ResumeLayout(false);
+            statusStrip1.PerformLayout();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private StatusStrip statusStrip1;
+        private ToolStripStatusLabel toolStripStatusLabel1;
+        private ToolStripStatusLabel toolStripStatusLabel2;
+        private ToolStripStatusLabel toolStripStatusLabel3;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem loadROMToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripMenuItem loadStateToolStripMenuItem;
+        private ToolStripMenuItem saveStateToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator2;
+        private ToolStripMenuItem quitToolStripMenuItem;
+        private ToolStripMenuItem emulationToolStripMenuItem;
+        private ToolStripMenuItem runToolStripMenuItem;
+        private ToolStripMenuItem pauseToolStripMenuItem;
+        private ToolStripMenuItem restartToolStripMenuItem;
     }
 }
--- Iris.UserInterface/MainWindow.cs
+++ Iris.UserInterface/MainWindow.cs
@@ -2,9 +2,70 @@ namespace Iris.UserInterface
 {
     public partial class MainWindow : Form
     {
+        private static readonly Dictionary<Keys, Emulation.GBA.Core.Keys> KeyMapping = new()
+        {
+            { Keys.A, Emulation.GBA.Core.Keys.A },
+            { Keys.Z, Emulation.GBA.Core.Keys.B},
+            { Keys.Space, Emulation.GBA.Core.Keys.Select},
+            { Keys.Enter, Emulation.GBA.Core.Keys.Start},
+            { Keys.Right, Emulation.GBA.Core.Keys.Right},
+            { Keys.Left, Emulation.GBA.Core.Keys.Left},
+            { Keys.Up, Emulation.GBA.Core.Keys.Up},
+            { Keys.Down, Emulation.GBA.Core.Keys.Down},
+            { Keys.S, Emulation.GBA.Core.Keys.R},
+            { Keys.Q, Emulation.GBA.Core.Keys.L},
+        };
+
+        private readonly Emulation.GBA.Core _GBA;
+
+        private int _frameCount = 0;
+        private readonly System.Timers.Timer _performanceUpdateTimer = new(1000);
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _GBA = new(DrawFrame);
+        }
+
+        private void DrawFrame(UInt16[] frameBuffer)
+        {
+
+        }
+
+        private void loadROMToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void loadStateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void saveStateToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void runToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pauseToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void restartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
