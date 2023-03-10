3a5689a1bd996a4995f27c357fb61a86ed0efa54
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2023-03-10 22:53:32 +0000

    emulation core: add SetKeyStatus method to ICore interface

--- Iris.EmulationCore/Common/ICore.cs
+++ Iris.EmulationCore/Common/ICore.cs
@@ -2,6 +2,28 @@
 {
     public delegate void DrawFrame_Delegate(UInt16[] frameBuffer);
 
+    public enum Key
+    {
+        A,
+        B,
+        Select,
+        Start,
+        Right,
+        Left,
+        Up,
+        Down,
+        R,
+        L,
+        X,
+        Y,
+    }
+
+    public enum KeyStatus
+    {
+        Input = 0,
+        NoInput = 1
+    }
+
     public interface ICore
     {
         public void Reset();
@@ -9,5 +31,6 @@
         public bool IsRunning();
         public void Run();
         public void Pause();
+        public void SetKeyStatus(Key key, KeyStatus status);
     }
 }
