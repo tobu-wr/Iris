19b8d83e2bec4bf109ba557f75448c1c2294f06c
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2023-07-29 11:38:03 +0100

    move NDS into its own project

---
+++ Iris.NDS/Memory.cs
@@ -0,0 +1,44 @@
+﻿namespace Iris.EmulationCore.NDS
+{
+    public sealed partial class Core
+    {
+        private const int KB = 1024;
+
+        private Byte[]? _ROM;
+
+        public void LoadROM(string filename)
+        {
+            _ROM = File.ReadAllBytes(filename);
+        }
+
+        private Byte ReadMemory8(UInt32 address)
+        {
+            throw new NotImplementedException("Iris.EmulationCore.NDS.Core.Memory: ReadMemory8 unimplemented");
+        }
+
+        private UInt16 ReadMemory16(UInt32 address)
+        {
+            throw new NotImplementedException("Iris.EmulationCore.NDS.Core.Memory: ReadMemory16 unimplemented");
+        }
+
+        private UInt32 ReadMemory32(UInt32 address)
+        {
+            throw new NotImplementedException("Iris.EmulationCore.NDS.Core.Memory: ReadMemory32 unimplemented");
+        }
+
+        private void WriteMemory8(UInt32 address, Byte value)
+        {
+            throw new NotImplementedException("Iris.EmulationCore.NDS.Core.Memory: ReadMemory32 unimplemented");
+        }
+
+        private void WriteMemory16(UInt32 address, UInt16 value)
+        {
+            throw new NotImplementedException("Iris.EmulationCore.NDS.Core.Memory: ReadMemory32 unimplemented");
+        }
+
+        private void WriteMemory32(UInt32 address, UInt32 value)
+        {
+            throw new NotImplementedException("Iris.EmulationCore.NDS.Core.Memory: ReadMemory32 unimplemented");
+        }
+    }
+}
