8797929e89db31d98230d4c7f940cddd0a98d100
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2023-07-29 12:24:11 +0100

    move GBA into its own project

---
+++ Iris.GBA/Core.Sound.cs
@@ -0,0 +1,43 @@
+﻿namespace Iris.EmulationCore.GBA
+{
+    public sealed partial class Core
+    {
+        private UInt16 _SOUND1CNT_L;
+        private UInt16 _SOUND1CNT_H;
+        private UInt16 _SOUND1CNT_X;
+
+        private UInt16 _SOUND2CNT_L;
+        private UInt16 _SOUND2CNT_H;
+
+        private UInt16 _SOUND3CNT_L;
+        private UInt16 _SOUND3CNT_H;
+        private UInt16 _SOUND3CNT_X;
+
+        private UInt16 _SOUND4CNT_L;
+        private UInt16 _SOUND4CNT_H;
+
+        private UInt16 _SOUNDCNT_L;
+        private UInt16 _SOUNDCNT_H;
+        private UInt16 _SOUNDCNT_X;
+
+        private UInt16 _SOUNDBIAS;
+
+        private UInt16 _WAVE_RAM0_L;
+        private UInt16 _WAVE_RAM0_H;
+
+        private UInt16 _WAVE_RAM1_L;
+        private UInt16 _WAVE_RAM1_H;
+
+        private UInt16 _WAVE_RAM2_L;
+        private UInt16 _WAVE_RAM2_H;
+
+        private UInt16 _WAVE_RAM3_L;
+        private UInt16 _WAVE_RAM3_H;
+
+        //private UInt16 _FIFO_A_L;
+        //private UInt16 _FIFO_A_H;
+
+        //private UInt16 _FIFO_B_L;
+        //private UInt16 _FIFO_B_H;
+    }
+}
