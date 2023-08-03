71ce6046da661c35ab2b5f44278e13b6208a310b
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2023-08-03 23:03:06 +0100

    implement scheduler

--- Iris.Common/Scheduler.cs
+++ Iris.Common/Scheduler.cs
@@ -2,27 +2,51 @@
 {
     public sealed class Scheduler
     {
-        public delegate void Event();
+        public delegate void Task_Delegate();
 
-        public bool HasEventToProcess()
+        private record struct TaskListEntry(UInt32 CycleCount, Task_Delegate Task);
+
+        private UInt32 _cycleCount;
+        private readonly List<TaskListEntry> _taskList = new();
+
+        public void Reset()
         {
-            // TODO
-            return false;
+            _cycleCount = 0;
+            _taskList.Clear();
         }
 
-        public void AddCycles(UInt32 cycles)
+        public void AddTask(UInt32 cycleCount, Task_Delegate task)
         {
-            // TODO
+            int i = 0;
+
+            while ((i < _taskList.Count) && (_taskList[i].CycleCount < cycleCount))
+                ++i;
+
+            _taskList.Insert(i, new(cycleCount, task));
         }
 
-        public void ProcessEvents()
+        public bool HasTaskReady()
         {
-            // TODO
+            return (_taskList.Count > 0) && (_taskList.First().CycleCount <= _cycleCount);
         }
 
-        public void AddEvent(UInt32 cycles, Event evt)
+        public void IncrementCycleCount(UInt32 cycleCount)
         {
+            _cycleCount += cycleCount;
+        }
 
+        public void ProcessTasks()
+        {
+            while (HasTaskReady())
+            {
+                _taskList.First().Task();
+                _taskList.RemoveAt(0);
+            }
+
+            for (int i = 0; i < _taskList.Count; ++i)
+                _taskList[i] = new(_taskList[i].CycleCount - _cycleCount, _taskList[i].Task);
+
+            _cycleCount = 0;
         }
     }
 }
