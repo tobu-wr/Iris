3dc494b002a6b00f2acd713712ba55f2c36f723e
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2023-08-03 12:57:19 +0100

    add scheduler (WIP)

---
+++ Iris.Common/Scheduler.cs
@@ -0,0 +1,28 @@
+﻿namespace Iris.Common
+{
+    public sealed class Scheduler
+    {
+        public delegate void Event();
+
+        public bool HasEventToProcess()
+        {
+            // TODO
+            return false;
+        }
+
+        public void AddCycles(UInt32 cycles)
+        {
+            // TODO
+        }
+
+        public void ProcessEvents()
+        {
+            // TODO
+        }
+
+        public void AddEvent(UInt32 cycles, Event evt)
+        {
+
+        }
+    }
+}
