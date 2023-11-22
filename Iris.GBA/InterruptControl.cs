139e0b78bb4f71c776b4f300f005b99528e348e8
Wilfried Rabouin <wilfried.rabouin@gmail.com>
2023-11-22 21:46:20 +0000

    naming consistency

--- Iris.GBA/InterruptControl.cs
+++ Iris.GBA/InterruptControl.cs
@@ -8,7 +8,7 @@ namespace Iris.GBA
         {
             VBlank = 1 << 0,
             //HBlank = 1 << 1,
-            //VCounterMatch = 1 << 2,
+            //VCountMatch = 1 << 2,
             //Timer0 = 1 << 3,
             //Timer1 = 1 << 4,
             //Timer2 = 1 << 5,
